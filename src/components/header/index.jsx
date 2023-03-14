fe0f993dda40651112679e8852ba7caf590ef897
Victor Eleanya <victoreleanya89@gmail.com>
2023-03-14 06:40:45 +0000

    updated mobile nav

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -28,18 +28,21 @@ const Header = () => {
           className="mobile-nav absolute top-[71px] px-8 py-8 bg-[#ffffff] left-0 right-0 flex flex-col items-center gap-[1.25rem] h-[15.8125rem] z-10 md:static md:h-4 md:top-0 md:py-0 md:flex-row md:px-0 md:gap-[2.3125rem]"
         >
           <NavLink
+            onClick={() => setShowMenu(false)}
             to="/stories"
             className="font-bold text-[0.9375rem] leading-[19.53px] text-[#000000] tracking-[2.5px] uppercase hover:text-[#dfdfdf] md:text-[0.75rem]"
           >
             stories
           </NavLink>
           <NavLink
+            onClick={() => setShowMenu(false)}
             to="/features"
             className="font-bold text-[0.9375rem] leading-[19.53px] text-[#000000] tracking-[2.5px] uppercase hover:text-[#dfdfdf] md:text-[0.75rem]"
           >
             features
           </NavLink>
           <NavLink
+            onClick={() => setShowMenu(false)}
             to="/prices"
             className="font-bold text-[0.9375rem] leading-[19.53px] text-[#000000] tracking-[2.5px] uppercase hover:text-[#dfdfdf] md:text-[0.75rem]"
           >
