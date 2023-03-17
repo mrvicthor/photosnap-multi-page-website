8806c0f40b8324da64000fc95a126296a3fbae4b
Victor Eleanya <victoreleanya89@gmail.com>
2023-03-17 20:40:13 +0000

    added new styles

--- src/components/common/linkbutton/index.jsx
+++ src/components/common/linkbutton/index.jsx
@@ -10,11 +10,6 @@ const LinkButton = ({ children, btnText }) => {
         {btnText}
       </div>
       {children}
-      {/* <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
-        <g fill="none" fillRule="evenodd" stroke="#ffffff">
-          <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-        </g>
-      </svg> */}
     </Link>
   );
 };
