feaf8d8480b1171e2c61e68c7204cfb4738e360e
Victor Eleanya <victoreleanya89@gmail.com>
2023-03-20 23:04:48 +0000

    designed mobile view for footer

--- src/components/common/linkbutton/index.jsx
+++ src/components/common/linkbutton/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const LinkButton = ({ children, btnText }) => {
+const LinkButton = ({ children, btnText, style }) => {
   return (
     <Link
       to="/"
-      className="flex items-center gap-x-[1.125rem] mt-[1.4375rem] md:mt-12 group"
+      className={`flex items-center gap-x-[1.125rem] ${
+        style ? style : "mt-[1.4375rem]"
+      } md:mt-12 group`}
     >
       <div className="text-[0.75rem] leading-[15.62px] font-bold uppercase hover:border-b hover:border-[#000000]">
         {btnText}
